f0b691c3e3becfe914351df5be5e3cb38b72f1fc
Dedi Sinaga <dedisinaga@gmail.com>
2017-09-29 08:52:37 +0700

    update ad_orm_process

--- ad_orm_process/orm_force.py
+++ ad_orm_process/orm_force.py
@@ -1,25 +1,26 @@
-from openerp.osv import fields,osv
+from openerp import api, fields, models, _
 from openerp import netsvc
 # from openerp.tools.safe_eval import safe_eval as eval
 
-class orm_force(osv.Model):
+class orm_force(models.Model):
 	_name = "orm.force"
-	_columns = {
-	"name"		: fields.char("Name",size=128,required=True),
-	"eval_text"	: fields.text("Eval Text ORM"),
-	"exec_text"	: fields.text("Exec Text ORM"),
-	"result"	: fields.text("Result"),
-	}
+	
+	name		= fields.Char("Name",size=128,required=True)
+	eval_text	= fields.Text("Eval Text ORM")
+	exec_text	= fields.Text("Exec Text ORM")
+	result		= fields.Text("Result")
+	
+	@api.multi
+	def execute_orm(self,):
 
-	def execute_orm(self,cr,uid,ids,context=None):
-		if not context:context={}
+		if not self._context:context={}
 		wf_service = netsvc.LocalService("workflow")
-		for force in self.browse(cr,uid,ids,context=context):
+		for force in self:
 			cp=compile(force.eval_text,'<string>', 'exec')
 			exec(cp)
 			if force.exec_text:
 				cp2=compile(force.exec_text,'<string>', 'exec')
 				exec(cp2)
-				print "result===============",result
-			self.write(cr,uid,force.id,{"result":result})
+
+			force.write({"result":result})
 		return True
